b27e7004d7de2627e85be30dd7ddecdcd84a641b
EmiKnape <enknape@aol.com>
2018-06-11 16:02:15 +0000

    automatically backed up by learn

--- dog.warmup.rb
+++ dog.warmup.rb
@@ -1,6 +1,10 @@
 class Dog 
- def bark
-   puts "woof!"
- end
-doggo=Dog.new 
-doggo.bark  
+  def name = (dog_name)
+    @this_dogs_name = dog_name
+  end
+  def name 
+    @this_dogs_name
+  end 
+end 
+doggo = Dog.new 
+doggo.name 
